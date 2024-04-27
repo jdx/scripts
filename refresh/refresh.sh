bb9198d4e55d2ceaa6f8886e60de358df060c4c8
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-04-27 18:49:54 -0500

    init

---
+++ refresh/refresh.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(mise activate bash --shims)"
+PATH="$HOME/.rustup/bin:$HOME/.cargo/bin:$PATH"
+
+brew upgrade
+rustup up
+cargo install-update --all
