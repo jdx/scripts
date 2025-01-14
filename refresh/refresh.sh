46cd9eb5cfef65728670492e58996ce27799b61e
jdx <216188+jdx@users.noreply.github.com>
2025-01-14 09:46:27 -0600

    update

--- refresh/refresh.sh
+++ refresh/refresh.sh
@@ -8,7 +8,3 @@ PATH="$HOME/.rustup/bin:$HOME/.cargo/bin:$PATH"
 brew upgrade
 rustup up
 cargo install-update --all
-
-if type -q brazil-package-cache 2>/dev/null; then
-  brazil-package-cache clean
-fi
